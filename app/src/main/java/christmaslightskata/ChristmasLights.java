1fbe479e29e436b93fab5c858494b4aa914dc39d
omrojasm <omrojas@gmail.com>
2022-10-30 12:48:42 +0100

    Refactor - Remove duplications

--- app/src/main/java/christmaslightskata/ChristmasLights.java
+++ app/src/main/java/christmaslightskata/ChristmasLights.java
@@ -1,5 +1,7 @@
 package christmaslightskata;
 
+import java.util.function.BiFunction;
+
 public class ChristmasLights {
 
     private boolean[][] lightIsOn = new boolean[1000][1000];
@@ -11,23 +13,38 @@ public class ChristmasLights {
     }
 
     public void turnOn(int startRow, int startCol, int endRow, int endCol) {
-        for (int i = startRow; i <= endRow; i++) {
-            for (int j = startCol; j <= endCol; j++) {
-                if (!lightIsOn[i][j]) {
-                    lightIsOn[i][j] = true;
-                    lightsOnCount++;
-                }
+        BiFunction<Integer, Integer, Void> onFunction = (i, j) -> {
+            if (!lightIsOn[i][j]) {
+                lightIsOn[i][j] = true;
+                lightsOnCount++;
             }
-        }
+            return null;
+        };
+        applyFunction(startRow, startCol, endRow, endCol, onFunction);
+
     }
 
     public void turnOff(int startRow, int startCol, int endRow, int endCol) {
+        BiFunction<Integer, Integer, Void> offFunction = (i, j) -> {
+            if (lightIsOn[i][j]) {
+                lightIsOn[i][j] = false;
+                lightsOnCount--;
+            }
+            return null;
+        };
+
+        applyFunction(startRow, startCol, endRow, endCol, offFunction);
+    }
+
+    private void applyFunction(
+            int startRow,
+            int startCol,
+            int endRow,
+            int endCol,
+            BiFunction<Integer, Integer, Void> function) {
         for (int i = startRow; i <= endRow; i++) {
             for (int j = startCol; j <= endCol; j++) {
-                if (lightIsOn[i][j]) {
-                    lightIsOn[i][j] = false;
-                    lightsOnCount--;
-                }
+                function.apply(i, j);
             }
         }
     }
